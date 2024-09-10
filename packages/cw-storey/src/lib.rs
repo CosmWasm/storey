6b8f213f07cfa91bbe725f8e12be994b67151159
Tomasz Kurcz <uint@users.noreply.github.com>
2024-09-10 10:45:44 +0200

    docs: fix indentation

--- packages/cw-storey/src/lib.rs
+++ packages/cw-storey/src/lib.rs
@@ -3,9 +3,9 @@
 //! This crate provides
 //! - a [*CosmWasm*] storage backend for use with [`storey`] collections,
 //! - a [*MessagePack*] encoding integration to be used for serializing and deserializing
-//! values, and
+//!   values, and
 //! - a set of container re-exports that remove the need to manually specify the
-//! encoding, instead relying on the default [*MessagePack*] encoding.
+//!   encoding, instead relying on the default [*MessagePack*] encoding.
 //!
 //! [*CosmWasm*]: https://github.com/CosmWasm/cosmwasm
 //! [*MessagePack*]: https://msgpack.org/
