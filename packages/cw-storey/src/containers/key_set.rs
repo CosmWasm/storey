65c1d06c59c534b2274f444964dcd44c60fef3c2
Tomasz Kurcz <uint@users.noreply.github.com>
2025-01-28 20:39:24 +0100

    impl Key traits for Uint128 etc

--- packages/cw-storey/src/containers/key_set.rs
+++ packages/cw-storey/src/containers/key_set.rs
@@ -1,5 +1,6 @@
-use cosmwasm_std::Addr;
-use storey::containers::map::{key::DynamicKey, Key, OwnedKey};
+use cosmwasm_std::{Addr, Int128, Int256, Int512, Int64, Uint128, Uint256, Uint512, Uint64};
+use storey::containers::map::key::{DynamicKey, FixedSizeKey, NumericKeyDecodeError};
+use storey::containers::map::{Key, OwnedKey};
 
 pub struct CwKeySet;
 
@@ -21,6 +22,101 @@ impl OwnedKey<CwKeySet> for Addr {
         <String as OwnedKey>::from_bytes(bytes).map(Addr::unchecked)
     }
 }
+
+macro_rules! cosmwasm_std_uints1 {
+    ($($ty:ty => $size:expr, $stdty:ty),*) => {
+        $(
+            impl Key<CwKeySet> for $ty {
+                type Kind = FixedSizeKey<$size>;
+
+                fn encode(&self) -> Vec<u8> {
+                    self.to_be_bytes().to_vec()
+                }
+            }
+
+            impl OwnedKey<CwKeySet> for $ty {
+                type Error = NumericKeyDecodeError;
+
+                fn from_bytes(bytes: &[u8]) -> Result<Self, Self::Error>
+                where
+                    Self: Sized,
+                {
+                    let array: [u8; $size] = bytes.try_into().map_err(|_| NumericKeyDecodeError::InvalidLength)?;
+                    Ok(<$stdty>::from_be_bytes(array).into())
+                }
+            }
+        )*
+    }
+}
+
+cosmwasm_std_uints1!(
+    Uint64 => 8, u64,
+    Uint128 => 16, u128
+);
+
+macro_rules! cosmwasm_std_uints2 {
+    ($($ty:ty => $size:expr),*) => {
+        $(
+            impl Key<CwKeySet> for $ty {
+                type Kind = FixedSizeKey<$size>;
+
+                fn encode(&self) -> Vec<u8> {
+                    self.to_be_bytes().to_vec()
+                }
+            }
+
+            impl OwnedKey<CwKeySet> for $ty {
+                type Error = NumericKeyDecodeError;
+
+                fn from_bytes(bytes: &[u8]) -> Result<Self, Self::Error>
+                where
+                    Self: Sized,
+                {
+                    let array: [u8; $size] = bytes.try_into().map_err(|_| NumericKeyDecodeError::InvalidLength)?;
+                    Ok(<$ty>::from_be_bytes(array))
+                }
+            }
+        )*
+    }
+}
+
+cosmwasm_std_uints2!(
+    Uint256 => 32,
+    Uint512 => 64
+);
+
+macro_rules! cosmwasm_std_ints {
+    ($($ty:ty => $size:expr),*) => {
+        $(
+            impl Key<CwKeySet> for $ty {
+                type Kind = FixedSizeKey<$size>;
+
+                fn encode(&self) -> Vec<u8> {
+                    let mut bytes = self.to_be_bytes();
+                    bytes[0] ^= 0x80;
+
+                    bytes.to_vec()
+                }
+            }
+
+            impl OwnedKey<CwKeySet> for $ty {
+                type Error = NumericKeyDecodeError;
+
+                fn from_bytes(bytes: &[u8]) -> Result<Self, Self::Error>
+                where
+                    Self: Sized,
+                {
+                    let mut array: [u8; $size] = bytes.try_into().map_err(|_| NumericKeyDecodeError::InvalidLength)?;
+                    array[0] ^= 0x80;
+
+                    Ok(<$ty>::from_be_bytes(array))
+                }
+            }
+        )*
+    }
+}
+
+cosmwasm_std_ints!(Int64 => 8, Int128 => 16, Int256 => 32, Int512 => 64);
 
 // delegate Key<CwKeySet> impls to their Key<DefaultKeySet> counterparts
 macro_rules! key_delegate {
@@ -91,5 +187,31 @@ impl<const N: usize> OwnedKey<CwKeySet> for [u8; N] {
         Self: Sized,
     {
         <Self as OwnedKey>::from_bytes(bytes)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn signed_ints() {
+        let nums = [
+            Int256::from(-542),
+            Int256::from(-111),
+            Int256::from(0),
+            Int256::from(121),
+            Int256::from(342),
+        ];
+
+        let mut byte_nums = nums.iter().map(|n| n.encode()).collect::<Vec<_>>();
+        byte_nums.sort();
+
+        let result = byte_nums
+            .iter()
+            .map(|bytes| Int256::from_bytes(bytes).unwrap())
+            .collect::<Vec<_>>();
+
+        assert_eq!(result, nums);
     }
 }
