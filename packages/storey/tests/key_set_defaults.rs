ab00228261118bd2e2cf7e7d1b9354ba61b7c866
Tomasz Kurcz <uint@users.noreply.github.com>
2025-01-30 14:42:32 +0100

    add tests for KeySetDefaults

---
+++ packages/storey/tests/key_set_defaults.rs
@@ -0,0 +1,54 @@
+use storey::containers::map::key::{Key, KeySetDefaults, OwnedKey};
+
+#[derive(KeySetDefaults)]
+pub struct MyKeySet;
+
+#[test]
+fn strings() {
+    let data = ["foo", "", "🧗🏼‍♀️hi"];
+
+    for s in data.iter() {
+        let encoded = <str as storey::containers::map::Key>::encode(s);
+        let my_encoded = <str as storey::containers::map::Key<MyKeySet>>::encode(s);
+        assert_eq!(encoded, my_encoded);
+    }
+
+    for s in data.iter() {
+        let s = s.to_string();
+
+        let encoded = <String as storey::containers::map::Key>::encode(&s);
+        let my_encoded = <String as storey::containers::map::Key<MyKeySet>>::encode(&s);
+        assert_eq!(encoded, my_encoded);
+
+        let my_decoded =
+            <String as storey::containers::map::OwnedKey<MyKeySet>>::from_bytes(&encoded).unwrap();
+        assert_eq!(s, my_decoded);
+    }
+
+    for s in data.iter() {
+        let s: Box<str> = (*s).into();
+
+        let encoded = <Box<str> as storey::containers::map::Key>::encode(&s);
+        let my_encoded = <Box<str> as storey::containers::map::Key<MyKeySet>>::encode(&s);
+        assert_eq!(encoded, my_encoded);
+
+        let my_decoded =
+            <Box<str> as storey::containers::map::OwnedKey<MyKeySet>>::from_bytes(&encoded)
+                .unwrap();
+        assert_eq!(s, my_decoded);
+    }
+}
+
+#[test]
+fn arrays() {
+    let data = [5u8; 99999];
+
+    let encoded = <[u8; 99999] as storey::containers::map::Key>::encode(&data);
+    let my_encoded = <[u8; 99999] as storey::containers::map::Key<MyKeySet>>::encode(&data);
+
+    assert_eq!(encoded, my_encoded);
+
+    let my_decoded =
+        <[u8; 99999] as storey::containers::map::OwnedKey<MyKeySet>>::from_bytes(&encoded).unwrap();
+    assert_eq!(data, my_decoded);
+}
